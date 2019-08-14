cd2c605f5c0e25198fa7380b0b39991126d130d3
Nata Vacheishvili <nvacheishvili@wpi.edu>
2019-08-14 17:23:17 -0400

    send html data back from server to the port 3000

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
     //event driven architecture
-    console.log(req);
+    // console.log(req);
+    //process.exit(); -> stop the event loop
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>asdasdad</title></head>');
+    res.write('<body><h1>Hello from Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
-
+ 
 server.listen(3000);
